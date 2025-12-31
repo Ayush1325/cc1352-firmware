c253564f1a934049fbe8fd665bbb1768c76771de
Ayush Singh <ayush@beagleboard.org>
2025-12-31 18:16:59 +0530

    Implement DNS-SD based discovery
    - Since all the problems with the upstream DNS-SD resolution are fixed,
      use it to bring back dynamic discovery.
    
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

--- src/tcp_discovery.c
+++ src/tcp_discovery.c
@@ -7,87 +7,89 @@
 #include "node.h"
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/net/dns_resolve.h>
 
 #define MAX_GREYBUS_NODES CONFIG_GREYBUS_APBRIDGE_CPORTS
 
 LOG_MODULE_DECLARE(cc1352_greybus, CONFIG_BEAGLEPLAY_GREYBUS_LOG_LEVEL);
 
-struct tcp_discovery_data {
-	int sock;
-};
+#ifdef CONFIG_BEAGLEPLAY_GREYBUS_MDNS_DISCOVERY
 
-static struct tcp_discovery_data tcp_discovery_data = {.sock = -1};
+static void handler(struct k_work *work);
 
-#ifndef CONFIG_BEAGLEPLAY_GREYBUS_MDNS_DISCOVERY
+static K_WORK_DEFINE(node_discovery, handler);
 
-static void handler(void *p1, void *p2, void *p3)
+static void cb(enum dns_resolve_status status, struct dns_addrinfo *info, void *user_data)
 {
-	static const char addr[] = CONFIG_BEAGLEPLAY_GREYBUS_NODE1;
-	struct sockaddr_in6 addr6;
-
-	net_ipaddr_parse(addr, sizeof(addr), (struct sockaddr *)&addr6);
-
-	while (1) {
-		k_msleep(NODE_DISCOVERY_INTERVAL);
-		node_filter(&addr6.sin6_addr, 1);
+	switch (status) {
+	case DNS_EAI_CANCELED:
+		LOG_DBG("Service request timeout");
+		k_work_submit(&node_discovery);
+		break;
+	case DNS_EAI_INPROGRESS:
+		if (info) {
+			// Ignore all other responses
+			if (info->ai_family == NET_AF_INET6) {
+				LOG_DBG("Got node address");
+				node_filter(&net_sin6(&info->ai_addr)->sin6_addr, 1);
+			}
+		}
+		break;
+	case DNS_EAI_ALLDONE:
+		LOG_DBG("All results received");
+		k_work_submit(&node_discovery);
+		break;
+	case DNS_EAI_FAIL:
+		LOG_DBG("No such name found.");
+		break;
+	default:
+		LOG_WRN("Unhandled status %d received (errno %d)", status, errno);
+		k_work_submit(&node_discovery);
 	}
 }
 
-#else
-
-static void handler(void *p1, void *p2, void *p3)
+static void handler(struct k_work *work)
 {
-	ARG_UNUSED(p1);
-	ARG_UNUSED(p2);
-	ARG_UNUSED(p3);
-
 	int ret;
-	struct in6_addr node_array[MAX_GREYBUS_NODES];
-	char query[] = "_greybus._tcp.local\0";
+	const char *query = "_greybus._tcp.local";
 
-	while (1) {
-		k_msleep(NODE_DISCOVERY_INTERVAL);
-
-		if (tcp_discovery_data.sock < 0) {
-			continue;
-		}
-
-		ret = mdns_query_send(tcp_discovery_data.sock, query, strlen(query));
-		if (ret < 0) {
-			LOG_WRN("Failed to get greybus nodes");
-			continue;
-		}
-
-		ret = mdns_query_recv(tcp_discovery_data.sock, node_array, MAX_GREYBUS_NODES, query,
-				      strlen(query), 2000);
-		if (ret < 0) {
-			LOG_WRN("Failed to get greybus nodes");
-			continue;
-		}
-
-		node_filter(node_array, ret);
+	ret = dns_resolve_service(dns_resolve_get_default(), query, NULL, cb, NULL,
+				  NODE_DISCOVERY_INTERVAL);
+	if (ret < 0) {
+		LOG_ERR("Cannot resolve DNS service (%d)", ret);
 	}
 }
-
 #endif // CONFIG_BEAGLEPLAY_GREYBUS_MDNS_DISCOVERY
-
-K_THREAD_DEFINE(tcp_discovery, 2048, handler, NULL, NULL, NULL, 6, 0, 0);
 
 void tcp_discovery_start(void)
 {
+#if CONFIG_BEAGLEPLAY_GREYBUS_STATIC_NODES_ENABLE
+	const char addr[] = CONFIG_BEAGLEPLAY_GREYBUS_STATIC_NODES;
+	struct sockaddr_in6 addr6;
+	int i, start;
+
+	for (i = 0, start = 0; i < ARRAY_SIZE(addr); i++) {
+		if (addr[i] == ',') {
+			net_ipaddr_parse(&addr[start], i - start, (struct sockaddr *)&addr6);
+			node_filter(&addr6.sin6_addr, 1);
+			start = i + 1;
+		}
+	}
+
+	if (i > start) {
+		net_ipaddr_parse(&addr[start], i - start, (struct sockaddr *)&addr6);
+		node_filter(&addr6.sin6_addr, 1);
+	}
+#endif // CONFIG_BEAGLEPLAY_GREYBUS_STATIC_NODES_ENABLE
+
 #ifdef CONFIG_BEAGLEPLAY_GREYBUS_MDNS_DISCOVERY
-	tcp_discovery_data.sock = mdns_socket_open_ipv6(&mdns_addr);
-#endif
-	k_thread_resume(tcp_discovery);
+	k_work_submit(&node_discovery);
+#endif // CONFIG_BEAGLEPLAY_GREYBUS_MDNS_DISCOVERY
 }
 
 void tcp_discovery_stop(void)
 {
-	k_thread_suspend(tcp_discovery);
 #ifdef CONFIG_BEAGLEPLAY_GREYBUS_MDNS_DISCOVERY
-	if (tcp_discovery_data.sock >= 0) {
-		mdns_socket_close(tcp_discovery_data.sock);
-		tcp_discovery_data.sock = -1;
-	}
-#endif
+	k_work_cancel(&node_discovery);
+#endif // CONFIG_BEAGLEPLAY_GREYBUS_MDNS_DISCOVERY
 }
